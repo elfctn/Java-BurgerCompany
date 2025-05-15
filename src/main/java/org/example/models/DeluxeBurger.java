4c1f4c82f80d91953f8f783ae8db477025f7b07b
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-15 15:51:12 +0300

    DeluxeBurger.java done

--- src/main/java/org/example/models/DeluxeBurger.java
+++ src/main/java/org/example/models/DeluxeBurger.java
@@ -1,4 +1,44 @@
 package org.example.models;
 
-public class DeluxeBurger {
+public class DeluxeBurger extends Hamburger {
+    private String cips;
+    private String drink;
+
+    public DeluxeBurger() {
+        super("Deluxe", "Double Sausage", 15.1, "WHITE");
+        this.cips = "Curvy";
+        this.drink = "Coke";
+
+        // Toplam 4.0 olacak şekilde eklemeler:
+        super.addHamburgerAddition1("Chips", 2.0);
+        super.addHamburgerAddition2("Drink", 2.0);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("\nDeluxe Burger için yeni malzeme eklenemez.");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("\nDeluxe Burger için yeni malzeme eklenemez.");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("\nDeluxe Burger için yeni malzeme eklenemez.");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("\nDeluxe Burger için yeni malzeme eklenemez.");
+    }
+
+    public String getCips() {
+        return cips;
+    }
+
+    public String getDrink() {
+        return drink;
+    }
 }
