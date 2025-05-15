acacea06bfc26266e1e23b7197b4e80d000e55b2
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-15 15:55:04 +0300

    maintest run- 1 test failed- will be checked

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.models.DeluxeBurger;
 import org.example.models.Hamburger;
+import org.example.models.HealthyBurger;
 
 public class Main {
     public static void main(String[] args) {
